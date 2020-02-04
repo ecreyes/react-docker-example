15328cf607902e0d4f4386df9cef1b1cefc02655
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2020-02-04 10:26:43 -0300

    function to arrow function in code registerPage

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 //styles and icons
 import '../css/animate.css';
 //import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,37 +11,40 @@ const registerStyle = {
     backgroundColor: "#247BA0",
     margin: 0
   },
-  submitButton:{ backgroundColor: "#FF1654", color: "white", borderRadius: 15 },
-  myLabelColor:{
-    color:"white"
+  submitButton: { backgroundColor: "#FF1654", color: "white", borderRadius: 15 },
+  myLabelColor: {
+    color: "white"
   },
-  divMiddlePosition:{ width: 100, height: 100, position: "absolute", borderRadius: 30, top: -60, zIndex: 10 },
-  formCard:{ backgroundColor: "#70C1B3", padding: 20, borderRadius: 15, paddingTop: 40 }
+  divMiddlePosition: { width: 100, height: 100, position: "absolute", borderRadius: 30, top: -60, zIndex: 10 },
+  formCard: { backgroundColor: "#70C1B3", padding: 20, borderRadius: 15, paddingTop: 40 },
+  errorColor: {
+    color: "#FF1654"
+  }
 }
 
 function RegisterPage() {
-  const [name,setName] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
-  const [passwordrepeat,setPasswordRepeat] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordrepeat, setPasswordRepeat] = useState("");
 
-  function handleNameChange(e) {
+  const handleNameChange = (e) => {
     setName(e.target.value);
   }
 
-  function handleEmailChange(e) {
+  const handleEmailChange = (e) => {
     setEmail(e.target.value);
   }
 
-  function handlePasswordChange(e) {
+  const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   }
 
-  function handlePasswordRepeatChange(e) {
+  const handlePasswordRepeatChange = (e) => {
     setPasswordRepeat(e.target.value);
   }
 
-  function handleSubmit(e) {
+  const onSubmit = (e) => {
     e.preventDefault();
     let state = {
       name,
@@ -51,61 +55,62 @@ function RegisterPage() {
     console.log(state);
   }
 
-    return (
-      <div className="jumbotron d-flex align-items-center vh-100" style={registerStyle.containerBackground}>
-        <div className="container" style={{ marginTop: 50 }} >
-          <div className="row d-flex justify-content-center">
-            <div className="col-12 col-md-4 animated fadeIn" style={registerStyle.formCard}>
-              <h2 className="d-flex align-items-center flex-column" style={registerStyle.myLabelColor}>Sign up now!</h2>
-              <form onSubmit={handleSubmit}>
-                <div className="d-flex justify-content-center">
-                  <div style={registerStyle.divMiddlePosition}>
-                    <img src={userAvatar} alt="icon user sign up" width="100" height="100" />
-                  </div>
+  return (
+    <div className="jumbotron d-flex align-items-center vh-100" style={registerStyle.containerBackground}>
+      <div className="container" style={{ marginTop: 50 }} >
+        <div className="row d-flex justify-content-center">
+          <div className="col-12 col-md-4 animated fadeIn" style={registerStyle.formCard}>
+            <h2 className="d-flex align-items-center flex-column" style={registerStyle.myLabelColor}>Sign up now!</h2>
+            <form onSubmit={onSubmit}>
+              <div className="d-flex justify-content-center">
+                <div style={registerStyle.divMiddlePosition}>
+                  <img src={userAvatar} alt="icon user sign up" width="100" height="100" />
                 </div>
-                <div className="form-group">
-                  <label htmlFor="name" style={registerStyle.myLabelColor}>Name</label>
-                  <input
-                    className="form-control"
-                    onChange={handleNameChange}
-                    value={name}
-                    type="text"
-                    id="name" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="email" style={registerStyle.myLabelColor}>Email</label>
-                  <input
-                    className="form-control"
-                    onChange={handleEmailChange}
-                    value={email}
-                    type="email"
-                    id="email" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password" style={registerStyle.myLabelColor}>Password</label>
-                  <input
-                    className="form-control"
-                    onChange={handlePasswordChange}
-                    value={password}
-                    type="password"
-                    id="password" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="passwordrepeat" style={registerStyle.myLabelColor}>Password Repeat</label>
-                  <input
-                    className="form-control"
-                    onChange={handlePasswordRepeatChange}
-                    value={passwordrepeat}
-                    type="password"
-                    id="passwordrepeat" />
-                </div>
-                <input type="submit" className="btn btn-block" style={registerStyle.submitButton} value="Sign up" />
-              </form>
-            </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="name" style={registerStyle.myLabelColor}>Name</label>
+                <input
+                  className="form-control"
+                  onChange={handleNameChange}
+                  value={name}
+                  type="text"
+                  id="name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" style={registerStyle.myLabelColor}>Email</label>
+                <input
+                  className="form-control"
+                  onChange={handleEmailChange}
+                  value={email}
+                  type="email"
+                  id="email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" style={registerStyle.myLabelColor}>Password</label>
+                <input
+                  className="form-control"
+                  onChange={handlePasswordChange}
+                  value={password}
+                  type="password"
+                  id="password" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="passwordrepeat" style={registerStyle.myLabelColor}>Password Repeat</label>
+                <input
+                  className="form-control"
+                  onChange={handlePasswordRepeatChange}
+                  value={passwordrepeat}
+                  type="password"
+                  id="passwordrepeat" />
+              </div>
+              <input type="submit" className="btn btn-block" style={registerStyle.submitButton} value="Sign up" />
+            </form>
           </div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
 
 export default RegisterPage;
